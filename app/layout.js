0aa12e28c19ca38cd20d318822fee4372582a9a3
Pharadol Chailod <bpc872@Pharadols-MacBook-Pro.local>
2026-07-15 11:11:34 +0700

    Update site favicon

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,14 @@ import "./globals.css";
 export const metadata = {
   title: "Pharadol Booking System",
   description: "Booking Management System",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon.png", type: "image/png", sizes: "512x512" },
+    ],
+    shortcut: ["/favicon.ico"],
+    apple: [{ url: "/apple-icon.png", type: "image/png", sizes: "180x180" }],
+  },
 };
 
 export default function RootLayout({ children }) {
